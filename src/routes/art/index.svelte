095c1fd042edc0bdd035c80f6bcbc7d16bd5b986
joefazz <joseph@fazzino.net>
2021-04-28 08:44:27 +0100

    in progress

---
+++ src/routes/art/index.svelte
@@ -0,0 +1,54 @@
+<svelte:head>
+  <title>Art | Fazz</title>
+</svelte:head>
+
+<h1 class="text-3xl font-bold">CodeXE</h1>
+<article>
+<span class="flex flex-row items-center justify-between">
+  <h2 class="text-xl font-thin uppercase">Code in the browser</h2>
+  <a href="https://codexe.run" target="self" class="text-lg underline text-theme">Link</a>
+</span>
+
+  <p class="my-2">
+    Next.js application built for my final year project of university. Gives
+    each user their own private coding environment to play with/break.
+  </p>
+
+  <p class="my-2">
+    Project was created with Next.js, React.js, Styled Components, Docker and
+    Node.js.
+  </p>
+
+  <b>Features</b>
+  <ul>
+    <li class="my-1 italic">- Private coding environment using containers</li>
+    <li class="my-1 italic">- Monaco code editing experience</li>
+    <li class="my-1 italic">- Support for JavaScript, Python and C++ compilation</li>
+    <li class="my-1 italic">- Ability to create and perform exercises ala Codecademy</li>
+  </ul>
+
+  <p class="my-2">
+    This project only has a deployed frontend no backend support is deployed, although cloning 
+    the repo and running it alongside the frontend should work.
+  </p>
+
+  <p class="my-2">
+    I also wrote a ~15 thousand word dissertation on this project available for
+    perusing.
+  </p>
+
+  <p class="links">
+    <a href="https://github.com/joefazz/CodeXE" target="self">
+      <code>Frontend</code>
+    </a>
+     - 
+     <a href="https://github.com/joefazz/Midgard" target="self">
+      <code>Backend</code>
+    </a>
+     - <a
+      href="https://github.com/joefazz/FYP_Report/blob/master/thesis.pdf"
+      target="self">
+      <code>Dissertation</code>
+    </a>
+  </p>
+</article>
